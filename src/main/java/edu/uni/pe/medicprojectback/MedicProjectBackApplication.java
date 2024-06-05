4e458ab93de4d6786e3a7d5c37e81411bfb40b68
Jefferson <jefferson.flores.m@uni.pe>
2024-06-05 00:30:30 -0500

    initial commit

---
+++ src/main/java/edu/uni/pe/medicprojectback/MedicProjectBackApplication.java
@@ -0,0 +1,13 @@
+package edu.uni.pe.medicprojectback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicProjectBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicProjectBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/uni/pe/medicprojectback/MedicProjectBackApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uni.pe.medicprojectback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicProjectBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
